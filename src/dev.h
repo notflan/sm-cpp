5346b0127e5c1cbeac897d0deeaaf8ef08258994
Avril <flanchan@cumallover.me>
2021-03-17 16:16:17 +0000

    added helper functions/macros

---
+++ src/dev.h
@@ -0,0 +1,24 @@
+#ifndef _TODO_H
+#define _TODO_H
+
+
+#ifdef __cplusplus
+#include <cstdio>
+[[noreturn]]
+#else
+#include <stdio.h>
+
+__attribute__((noreturn))
+#endif
+inline static void _dev__unimplemented(const char* msg)
+{
+		fputs("unimplemented", stderr);
+	if(msg) fputs(msg, stderr);
+	fputc('\n', stderr);
+
+	abort();
+}
+
+#define TODO(msg) _dev__unimplemented(": TODO: " msg)
+
+#endif /* _TODO_H */
--- src/state.cpp
+++ src/state.cpp
@@ -1,6 +1,13 @@
 #include <state.h>
+#include "dev.h"
+
+void _sm_pop_stack(sm_state* state)
+{
+	TODO("Free the current stack frame in `state` and replace it with the previous one");
+}
 
 sm_yield _sm_noop(sm_state*)
 {
 	return (sm_yield)nullptr;
 }
+
